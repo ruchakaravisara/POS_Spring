aa3e62bc4279cdf3d05361f3ae68bf405d13b885
ruchakaravisara <ruchakaravisara1@gmail.com>
2023-10-11 04:14:19 +0530

    order and orderdetail controllers create

---
+++ BackEnd/src/main/java/lk/ijse/spring/pos/controller/PurchaseOrderController.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.pos.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+@CrossOrigin
+public class PurchaseOrderController {
+}
